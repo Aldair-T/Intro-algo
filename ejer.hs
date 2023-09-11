2b1a758fd3726e1f14741ab6ab441aa7e6598069
Aldair Torres <86437315+Aldair-T@users.noreply.github.com>
2023-09-11 13:47:05 -0300

    Update ejer.hs

--- ejer.hs
+++ ejer.hs
@@ -68,4 +68,87 @@ iesimoDigito n i | i == cantDigitos n = ultimoDigito n
                  where sacarUltimo n = div n 10
                         ultimoDigito n = mod n 10
 
+                          -- Guia 4 ejer 13 --
+
+{- f(n,m) sumatoria de i=1 a n, sumatoria de j=1 a n con i**j -}
+
+{-
+sumaInterna :: Integer -> Integer -> Integer
+sumaInterna 
+
+ejer13 :: Integer -> Integer -> Integer
+ejer13 n m | n == 0 = 0 
+           | otherwise = ejer13(n-1, m) + 
+-}
+
+
+
+
+menorDivisor :: Integer -> Integer
+menorDivisor x | x == 1 = 1
+               | otherwise = divisor x 2
+
+divisor :: Integer -> Integer -> Integer
+divisor x y | x == y = x
+            | mod x y == 0 = y
+            | otherwise = divisor x (y+1)
+
+
+
+{-
+esSumadePrimos :: Integer -> Bool
+esSumadePrimos n = restaPrimos n 2
+
+restaPrimos:: Integer -> Integer -> Bool
+restaPrimos n i | n == 0 = True
+                | n <= 0 = False
+                | otherwise = restaPrimos (n - primos i) (i + 1)
+
+primos :: Integer -> Integer
+primos n | menorDivisor n /= n = 0
+         | otherwise n 
+
+-}
+
+{-
+
+                LISTAS
+
+head: saca el primer elemento
+tail: lista sin el primer elemento
+(x:xs): se divide entre el head == x y el tail == xs
+0:[1,2,3]: concatena -> [0,1,2,3]
+[1,2,3]++[0]: concatena -> [1,2,3,0]
+(x:y:z:xs): se puede hacer pero puede dar indefinido
+
+-}
+
+longitud :: [t] -> Integer
+longitud [] = 0
+longitud (_:tail) = 1 + longitud tail
+
+
+{-
+Eq(t): familia de tipos las cuales pueden tener una igualdad
+-}
+
+comparar :: t -> [s] -> Bool
+comparar - [] = False
+comparar n (x:xs) | n == s = True
+                  | otherwise = comparar n xs
+
+hayRepetidos :: (Eq t) -> [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) = comparar x xs || hayRepetidos xs
+{-hayRepetidos s | s == [] = False
+               | comparar (head s, tail s) || hayRepetidos (tail s)-}
+
+
+
+maximo :: [Integer] -> Integer
+maximo []
+maximo [x] = x
+maximo (x:y:s) | x >= y = maximo(x:s)
+               | x < y = maximo(y:s)
+
                         
